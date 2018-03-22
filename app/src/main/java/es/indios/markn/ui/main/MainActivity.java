479ec61796408ada1edd547edc8738573b6b76a3
Guillermo Rial Salgueiro <guillermorial@imasdetres.com>
2018-03-22 09:31:20 +0100

    fixes

--- app/src/main/java/es/indios/markn/ui/main/MainActivity.java
+++ app/src/main/java/es/indios/markn/ui/main/MainActivity.java
@@ -1,7 +1,5 @@
 package es.indios.markn.ui.main;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,32 +14,16 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import es.indios.markn.blescanner.Scanner;
-import es.indios.markn.data.SyncService;
 import es.indios.ribot.androidboilerplate.R;
 import es.indios.markn.ui.base.BaseActivity;
 
 public class MainActivity extends BaseActivity implements MainMvpView {
-
-    private static final String EXTRA_TRIGGER_SYNC_FLAG =
-            "MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
-
     @Inject MainPresenter mMainPresenter;
     @Inject BeaconsAdapter mBeaconsAdapter;
 
     @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
     @BindView(R.id.main_beacon_textview)
     TextView mTextView;
-
-    /**
-     * Return an Intent to start this Activity.
-     * triggerDataSyncOnCreate allows disabling the background sync service onCreate. Should
-     * only be set to false during testing.
-     */
-    public static Intent getStartIntent(Context context, boolean triggerDataSyncOnCreate) {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra(EXTRA_TRIGGER_SYNC_FLAG, triggerDataSyncOnCreate);
-        return intent;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,18 +36,14 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mMainPresenter.attachView(this);
         Scanner.getInstance().subscribeListener(mMainPresenter);
-
-        if (getIntent().getBooleanExtra(EXTRA_TRIGGER_SYNC_FLAG, true)) {
-            startService(SyncService.getStartIntent(this));
-        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
+        Scanner.getInstance().deleteListener(mMainPresenter);
         mMainPresenter.detachView();
-        Scanner.getInstance().deleteListeners();
     }
 
     /***** MVP View methods implementation *****/
